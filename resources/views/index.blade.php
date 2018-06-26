36d16888bacf3f5e32dd55c398328629f19563dc
yota-hada <hadayota33@gmail.com>
2018-06-26 23:06:32 +0900

    feat: add views/layouts.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+@include('layouts.header')
+@include('layouts.footer')
+
+@section('pageCss')
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('pageJs')
+<script type="text/javascript">
+</script>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+@section('footer')
+@yield('pageJs')
+@endsection
