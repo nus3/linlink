3ba5f63b9b75a2d168902bf898b49323c37b0c7e
yota-hada <hadayota33@gmail.com>
2018-06-27 22:09:10 +0900

    feat: add link's index into views and add link's controller.

--- resources/views/link/index.blade.php
+++ resources/views/link/index.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('content')
-
+HelloWorld
 @endsection
 
 @section('pageJs')
---
+++ resources/views/link/links.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+@include('layouts.header')
+@include('layouts.footer')
+
+@section('pageCss')
+@endsection
+
+@section('content')
+HelloWorld
+@endsection
+
+@section('pageJs')
+<script type="text/javascript">
+</script>
+@endsection
