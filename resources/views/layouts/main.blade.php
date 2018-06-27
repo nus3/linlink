4633d56a5e325f25c937908392f71d30f421154c
yota-hada <hadayota33@gmail.com>
2018-06-27 21:27:28 +0900

    chore: add jquery and materialize.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name = "csrf-token" content = "{{ @csrf_token }}">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="/css/materialize.min.css" type="text/css" rel="stylesheet"/>
     <link href="/css/base.css" type="text/css" rel="stylesheet"/>
     @yield('pageCss')
     <title>gamba</title>
@@ -14,6 +16,8 @@
     @yield('content')
 
 
+    <script type="text/javascript" src="/js/vendor/jquery3.3.1.js"></script>
+    <script type="text/javascript" src="/js/vendor/materialize.min.js"></script>
     @yield('footer')
 </body>
 </html>
