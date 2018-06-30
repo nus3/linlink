15b8b115577f2d131f4aab784d28c8a6b3077800
yota-hada <hadayota33@gmail.com>
2018-06-30 19:51:56 +0900

    style: change sidebar.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,10 +1,10 @@
 @section('sidebar')
 <div class="sidebar cyan darken-3">
     <div class="collection">
-        <!-- TODO: hoverの時のアニメーション追加 -->
-        <a href="" class="collection-item cyan darken-3">Home</a>
-        <a href="" class="collection-item cyan darken-3">シェアする</a>
-        <a href="" class="collection-item cyan darken-3">探す</a>
+        <!-- TODO: hoverの時の背景色の変化 -->
+        <a href="" class="collection-item">Home</a>
+        <a href="" class="collection-item">シェアする</a>
+        <a href="" class="collection-item">探す</a>
     </div>
 </div>
 @endsection
