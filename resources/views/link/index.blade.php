a35443647541e88421e9e32a574510286fbd13a4
yota-hada <hadayota33@gmail.com>
2018-07-02 23:25:32 +0900

    style: add responsive index-top.

--- resources/views/link/index.blade.php
+++ resources/views/link/index.blade.php
@@ -13,6 +13,9 @@
     <div class="contents">
         <div class="index-top">
             <h3 class="index-top__title">お気に入りのリンクをシェアしよう</h3>
+            <div class="index-top__btn-wrapper">
+                <button class="btn waves-effect waves-light cyan darken-3 index-top__btn">シェアする</button>
+            </div>
         </div>
     </div>
 
