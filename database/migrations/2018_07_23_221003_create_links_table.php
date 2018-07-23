f63c24c084b60b8c59e65f46a3a8698482e88700
yota-hada <hadayota33@gmail.com>
2018-07-23 22:38:13 +0900

    feat: add migration files.

---
+++ database/migrations/2018_07_23_221003_create_links_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('key', 32)->unique();
+
+            $table->text('url')->nullable();
+            $table->text('description')->nullable();
+            $table->string('name', 256)->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
---
+++ database/migrations/2018_07_23_222512_create_accesses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accesses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('link_id')->unsigned();
+            $table->unsignedInteger('session_id')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->index(['link_id', 'session_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('access');
+    }
+}
