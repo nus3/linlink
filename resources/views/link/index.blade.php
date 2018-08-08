8f11049ba7da62433c02eadfb8786805830ac316
yota-hada <hadayota33@gmail.com>
2018-08-08 22:38:33 +0900

    style: fix link index.

--- resources/views/link/index.blade.php
+++ resources/views/link/index.blade.php
@@ -21,33 +21,35 @@
         </div>
 
         <div class="section">
-            <h4 class="section__title">アクセスランキング　Best5</h4>
+            <h4 class="section__title">アクセスランキング</h4>
             <div class="rank-section__items">
                 @foreach ($popularLinks as $popularLink)
                     <div class="card rank-section__item">
                         <div class="card-image">
                             <!-- TODO: 画像のサイズがバラバラなるけどもどうするか -->
-                            <!-- TODO: OGP画像がなかったら時の画像を作る -->
                             @if(is_null($popularLink->ogp_url))
-                                <img src="/img/dummy.jpg">
+                                <img src="/img/no_image.png">
                             @else
                                 <img src="{{ $popularLink->ogp_url }}">
                             @endif
-                            <!-- TODO: もうちょっとランキングを目立たせる -->
-                            <span class="card-title">No{{$loop->iteration}}</span>
+                            <a class="btn-floating halfway-fab cyan darken-3 rank-section__item-count" href="javascript:void(0)">No{{$loop->iteration}}</a>
                         </div>
                         <div class="card-content">
                             <!-- TODO: 文字数によるフッタの大きさを統一 -->
+                            <h5 class="rank-section__item-access">
+                                <i class="material-icons cyan-text text-darken-3 rank-section__item-access-icon ">visibility</i>
+                                {{ $popularLink->accesses_count }}アクセス
+                            </h5>
                             <p>{{ $popularLink->description }}</p>
                         </div>
-                        <div class="card-action rank-section__item-footer">
-                            <!-- TODO: リンク先へ遷移する前にアクセスレコードを作成する -->
+                        <!-- TODO: リンク先へ遷移する前にアクセスレコードを作成する -->
+                        <!-- <div class="card-action rank-section__item-footer">
                             <a href="{{ $popularLink->url }}" target="_blank">リンクへ</a>
                             <p class="rank-section__item-access">
                                 <i class="material-icons rank-section__item-access-icon">visibility</i>
                                 {{ $popularLink->accesses_count }}
                             </p>
-                        </div>
+                        </div> -->
                     </div>
                 @endforeach
             </div>
