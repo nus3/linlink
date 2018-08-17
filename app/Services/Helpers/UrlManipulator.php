3f45b6e5011a09d55aeb54bbb129e0d2bcdf865f
yota-hada <hadayota33@gmail.com>
2018-08-17 16:21:28 +0900

    feat: add convertRelativeToAbsolute function.

--- app/Services/Helpers/UrlManipulator.php
+++ app/Services/Helpers/UrlManipulator.php
@@ -18,9 +18,53 @@ class UrlManipulator
     {
         $baseParse = parse_url($baseUrl);
         $relativeParse = parse_url($relativeUrl);
+                
+        $absoluteUrl = '';
+        if (isset($relativeParse['host'])) {
+            $absolutePath = "{$relativeParse['scheme']}://{$relativeParse['host']}{$relativeParse['path']}";
+            $absoluteQuery = isset($relativeParse['query']) ? "?{$relativeParse['query']}" : '';
+            $absoluteUrl = $absolutePath. $absoluteQuery;
+        }
+        elseif (isset($relativeParse['path'])) {
 
-        logger(print_r($baseParse, true));
-        logger(print_r($relativeParse, true));
+            $pathInitialTwo = substr($relativeParse['path'], 0, 2);
+            $pathInitialOne = substr($relativeParse['path'], 0, 1);
+            if (in_array($pathInitialTwo, ['./', '..'])) {
+                $pathArray = explode('/', $relativeParse['path']);
 
+                $basePath = isset($baseParse['path']) ? $baseParse['path'] : '';
+                if (pathinfo($basePath, PATHINFO_EXTENSION)) {
+                    $basePath = pathinfo($basePath, PATHINFO_DIRNAME);
+                }
+
+                foreach ($pathArray as $path) {
+                    if ($path == '..') {
+                        $basePath = dirname($basePath);
+                    }
+                    if (in_array($basePath, ['/', '\\'])) {
+                        $basePath = '';
+					    break;
+                    }
+                }
+
+                $relativeUrl = str_replace(['../','./'], '', $relativeUrl);
+
+                $absoluteUrl = "{$baseParse['scheme']}://{$baseParse['host']}{$basePath}/{$relativeUrl}";
+            }
+            elseif ($pathInitialOne == '/') {
+                $absoluteUrl = $baseParse['scheme'] . '://' . $baseParse['host'] . $relativeUrl;
+            }
+            else {
+                $basePath = isset($baseParse['path']) ? $baseParse['path'] : '';
+
+                if (pathinfo($basePath, PATHINFO_EXTENSION)) {
+                    $basePath = pathinfo($basePath, PATHINFO_DIRNAME);
+                }
+                $absolutePath = $baseParse['host'] . '/' . $basePath . '/' . $relativeUrl;
+                $absoluteUrl = $baseParse['scheme'] . '://' . str_replace('//', '/', $absolutePath);
+            }
+        }
+
+        return $absoluteUrl;
     }
 }
